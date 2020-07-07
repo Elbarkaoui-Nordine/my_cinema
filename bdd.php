0548a4ca8d32c7fbe1a3440ca3dc3c3b566817e1
Nordine <nordine.el-barkaoui@epitech.eu>
2020-07-07 19:28:13 +0200

    bdd changes

--- bdd.php
+++ bdd.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-    $dbh = new PDO("mysql:host=eu-cdbr-west-03.cleardb.net;port=3306;dbname=my_cinema","b777649fa1ef37","ab0e71bd",[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    $dbh = new PDO("mysql:host=eu-cdbr-west-03.cleardb.net;port=3306;dbname=heroku_7a44925b22e14d6","b777649fa1ef37","ab0e71bd",[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
 }
 catch (PDOException $e) {
