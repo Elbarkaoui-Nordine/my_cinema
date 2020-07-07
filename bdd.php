f8c9b425699cce20b9dcbf1ff70441a670800478
Nordine <nordine.el-barkaoui@epitech.eu>
2020-07-07 19:01:09 +0200

    bdd change

--- bdd.php
+++ bdd.php
@@ -1,6 +1,7 @@
 <?php
+
 try{
-    $dbh = new PDO('mysql:host=127.0.0.1;dbname=cinema','root',"",[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    $dbh = new PDO('mysql:host=eu-cdbr-west-03.cleardb.net;dbname=my_cinema','b343eb1c6f7662',"fd1f61ad",[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
 }
 catch (PDOException $e) {
